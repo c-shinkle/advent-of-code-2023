2f9055993cc39d6c11eeb2f27b9eb862b1356316
Christian Shinkle <cshinkle2018@gmail.com>
2024-02-24 13:02:02 -0600

    more tweaks

--- day_3/src/gear_ratio.rs
+++ day_3/src/gear_ratio.rs
@@ -1,10 +1,10 @@
 use std::cmp::Ordering;
 use std::collections::HashSet;
+use std::ops::Range;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone)]
 struct Location {
-    start: usize,
-    end: usize,
+    range: Range<usize>,
     row: usize,
 }
 
@@ -21,73 +21,64 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
             let start = substring.find(part_str).unwrap() + previous_byte_offset;
             previous_byte_offset = start + part_str.len();
             all_locations.push(Location {
-                start,
-                end: start + part_str.len(),
+                range: start..start + part_str.len(),
                 row,
             });
         }
     }
 
     let lines: Vec<&str> = input.lines().collect();
-    input
-        .lines()
+
+    let mut sum = 0;
+    for (asterisk_row, asterisk_col) in lines
+        .iter()
         .enumerate()
-        .flat_map(|(row, line)| {
-            line
-                .match_indices('*')
-                .map(move |(index, _)| (row, index))
+        .flat_map(|(row, line)| line.match_indices('*').map(move |(index, _)| (row, index)))
+    {
+        let set: HashSet<Location> = [
+            (Some(asterisk_row), asterisk_col.checked_add(1)),
+            (asterisk_row.checked_sub(1), asterisk_col.checked_add(1)),
+            (asterisk_row.checked_sub(1), Some(asterisk_col)),
+            (asterisk_row.checked_sub(1), asterisk_col.checked_sub(1)),
+            (Some(asterisk_row), asterisk_col.checked_sub(1)),
+            (asterisk_row.checked_add(1), asterisk_col.checked_sub(1)),
+            (asterisk_row.checked_add(1), Some(asterisk_col)),
+            (asterisk_row.checked_add(1), asterisk_col.checked_add(1)),
+        ]
+        .into_iter()
+        .filter_map(|(row, col)| Some((row?, col?)))
+        .flat_map(|(row, col)| {
+            all_locations.iter().map(move |location| {
+                let Location {
+                    range,
+                    row: digit_row,
+                } = location;
+                (row == *digit_row && range.contains(&col)).then_some(location.clone())
+            })
         })
-        .map(|(asterisk_row, asterisk_col)| {
-            let mut set: HashSet<Location> = HashSet::new();
-            [
-                (Some(asterisk_row), asterisk_col.checked_add(1)),
-                (asterisk_row.checked_sub(1), asterisk_col.checked_add(1)),
-                (asterisk_row.checked_sub(1), Some(asterisk_col)),
-                (asterisk_row.checked_sub(1), asterisk_col.checked_sub(1)),
-                (Some(asterisk_row), asterisk_col.checked_sub(1)),
-                (asterisk_row.checked_add(1), asterisk_col.checked_sub(1)),
-                (asterisk_row.checked_add(1), Some(asterisk_col)),
-                (asterisk_row.checked_add(1), asterisk_col.checked_add(1)),
-            ]
-            .into_iter()
-            .filter_map(|(row, col)| Some((row?, col?)))
-            .for_each(|(row, col)| {
-                for location in all_locations.iter() {
-                    let Location {
-                        start,
-                        end,
-                        row: digit_row,
-                    } = location;
-                    let range = *start..*end;
-                    if row == *digit_row && range.contains(&col) {
-                        set.insert(*location);
-                    }
-                }
-            });
+        .flatten()
+        .collect();
 
-            match set.len().cmp(&2) {
-                Ordering::Less => 0,
-                Ordering::Equal => {
-                    let mut set_iter = set.into_iter();
+        match set.len().cmp(&2) {
+            Ordering::Less => {}
+            Ordering::Equal => {
+                let mut set_iter = set.into_iter();
 
-                    let first_number_location = set_iter.next().unwrap();
-                    let first_number_row = lines[first_number_location.row];
-                    let first_number_str = &first_number_row[first_number_location.start..first_number_location.end];
-                    let first_number = first_number_str.parse::<u32>().unwrap();
+                let Location { row, range } = set_iter.next().unwrap();
+                let first_number_str = &lines[row][range];
+                let first_number = first_number_str.parse::<u32>().unwrap();
 
-                    let second_number_location = set_iter.next().unwrap();
-                    let second_number_row = lines[second_number_location.row];
-                    let second_number_str = &second_number_row[second_number_location.start..second_number_location.end];
-                    let second_number = second_number_str.parse::<u32>().unwrap();
+                let Location { row, range } = set_iter.next().unwrap();
+                let second_number_str = &lines[row][range];
+                let second_number = second_number_str.parse::<u32>().unwrap();
 
-                    first_number * second_number
-                },
-                Ordering::Greater => panic!(
-                    "More than two numbers found adjacent to asterisk at row {asterisk_row} col {asterisk_col}"
-                ),
+                sum += first_number * second_number;
             }
-        })
-        .sum()
+            Ordering::Greater => unreachable!(),
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
