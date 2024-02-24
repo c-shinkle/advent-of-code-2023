d8fc7387c91b61ab3b22d3a960c6f6644cdfff8d
Christian Shinkle <cshinkle2018@gmail.com>
2024-02-24 13:53:23 -0600

    replace vec with hash map, 75% less runtime!

--- day_3/src/gear_ratio.rs
+++ day_3/src/gear_ratio.rs
@@ -1,16 +1,16 @@
 use std::cmp::Ordering;
-use std::collections::HashSet;
-use std::ops::Range;
+use std::collections::{HashMap, HashSet};
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 struct Location {
-    range: Range<usize>,
+    start: usize,
+    end: usize,
     row: usize,
 }
 
 pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
     input = input.trim();
-    let mut all_locations: Vec<Location> = Vec::new();
+    let mut all_locations: HashMap<(usize, usize), Location> = HashMap::new();
     for (row, line) in input.lines().enumerate() {
         let mut previous_byte_offset = 0;
         for part_str in line
@@ -20,10 +20,11 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
             let substring = &line[previous_byte_offset..line.len()];
             let start = substring.find(part_str).unwrap() + previous_byte_offset;
             previous_byte_offset = start + part_str.len();
-            all_locations.push(Location {
-                range: start..start + part_str.len(),
-                row,
-            });
+            let end = start + part_str.len();
+            let location = Location { start, end, row };
+            for i in start..end {
+                all_locations.insert((row, i), location);
+            }
         }
     }
 
@@ -35,7 +36,7 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
         .enumerate()
         .flat_map(|(row, line)| line.match_indices('*').map(move |(index, _)| (row, index)))
     {
-        let set: HashSet<Location> = [
+        let set: HashSet<&Location> = [
             (Some(asterisk_row), asterisk_col.checked_add(1)),
             (asterisk_row.checked_sub(1), asterisk_col.checked_add(1)),
             (asterisk_row.checked_sub(1), Some(asterisk_col)),
@@ -46,17 +47,7 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
             (asterisk_row.checked_add(1), asterisk_col.checked_add(1)),
         ]
         .into_iter()
-        .filter_map(|(row, col)| Some((row?, col?)))
-        .flat_map(|(row, col)| {
-            all_locations.iter().map(move |location| {
-                let Location {
-                    range,
-                    row: digit_row,
-                } = location;
-                (row == *digit_row && range.contains(&col)).then_some(location.clone())
-            })
-        })
-        .flatten()
+        .filter_map(|(row, col)| all_locations.get(&(row?, col?)))
         .collect();
 
         match set.len().cmp(&2) {
@@ -64,12 +55,12 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
             Ordering::Equal => {
                 let mut set_iter = set.into_iter();
 
-                let Location { row, range } = set_iter.next().unwrap();
-                let first_number_str = &lines[row][range];
+                let Location { row, start, end } = *set_iter.next().unwrap();
+                let first_number_str = &lines[row][start..end];
                 let first_number = first_number_str.parse::<u32>().unwrap();
 
-                let Location { row, range } = set_iter.next().unwrap();
-                let second_number_str = &lines[row][range];
+                let Location { row, start, end } = *set_iter.next().unwrap();
+                let second_number_str = &lines[row][start..end];
                 let second_number = second_number_str.parse::<u32>().unwrap();
 
                 sum += first_number * second_number;
