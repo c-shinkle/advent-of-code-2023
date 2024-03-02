3906bd7d0dc4800678a56fb2b6730da826d7c69a
Christian Shinkle <cshinkle2018@gmail.com>
2024-03-01 18:13:31 -0600

    added back original gear ratio solution (somehow current solution got slower)

--- day_3/benches/bench.rs
+++ day_3/benches/bench.rs
@@ -6,7 +6,7 @@ extern crate test;
 mod tests {
     use day_3::{
         all_parts::{get_all_part_numbers_func, no_vecs_or_ndarray_or_regex},
-        gear_ratio::get_all_gear_ratios_func,
+        gear_ratio::*,
         input::INPUT,
     };
     use test::{black_box, Bencher};
@@ -38,7 +38,18 @@ mod tests {
         let mut sum = u32::default();
 
         b.iter(|| {
-            sum = black_box(get_all_gear_ratios_func(INPUT));
+            sum = black_box(first_impl_gear_ratios(INPUT));
+        });
+
+        assert_eq!(sum, 80253814);
+    }
+
+    #[bench]
+    fn hashmap_locations_no_hashset_bench(b: &mut Bencher) {
+        let mut sum = u32::default();
+
+        b.iter(|| {
+            sum = black_box(hashmap_locations_no_hashset_gear_ratios(INPUT));
         });
 
         assert_eq!(sum, 80253814);
--- day_3/src/gear_ratio.rs
+++ day_3/src/gear_ratio.rs
@@ -1,13 +1,103 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
 
-type Location = (u16, u16, u16);
+use regex::Regex;
 
-pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Location {
+    start: usize,
+    end: usize,
+    row: usize,
+}
+
+type LocationTuple = (u16, u16, u16);
+
+pub fn first_impl_gear_ratios(mut input: &str) -> u32 {
+    let digit_regex = Regex::new(r"(\d+)").unwrap();
+    input = input.trim();
+    let matrix: Vec<&str> = input.lines().collect();
+    let all_locations: Vec<Location> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            digit_regex
+                .find_iter(line)
+                .map(move |capture| (row, capture))
+        })
+        .map(|(row, capture)| Location {
+            start: capture.start(),
+            end: capture.end(),
+            row,
+        })
+        .collect();
+    let asterisk_regex = Regex::new(r"(\*)").unwrap();
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            asterisk_regex
+                .find_iter(line)
+                .map(move |capture| (row, capture.start()))
+        })
+        .map(|(asterisk_row, asterisk_col)| {
+            let mut set: HashSet<Location> = HashSet::new();
+            [
+                (Some(asterisk_row), asterisk_col.checked_add(1)),
+                (asterisk_row.checked_sub(1), asterisk_col.checked_add(1)),
+                (asterisk_row.checked_sub(1), Some(asterisk_col)),
+                (asterisk_row.checked_sub(1), asterisk_col.checked_sub(1)),
+                (Some(asterisk_row), asterisk_col.checked_sub(1)),
+                (asterisk_row.checked_add(1), asterisk_col.checked_sub(1)),
+                (asterisk_row.checked_add(1), Some(asterisk_col)),
+                (asterisk_row.checked_add(1), asterisk_col.checked_add(1)),
+            ]
+            .into_iter()
+            .filter_map(|(row, col)| Some((row?, col?)))
+            .for_each(|(row, col)| {
+                for location in all_locations.iter() {
+                    let Location {
+                        start,
+                        end,
+                        row: digit_row,
+                    } = location;
+                    let range = *start..*end;
+                    if row == *digit_row && range.contains(&col) {
+                        set.insert(*location);
+                    }
+                }
+            });
+
+            match set.len().cmp(&2) {
+                Ordering::Less => 0,
+                Ordering::Equal => {
+                    let mut set_iter = set.into_iter();
+
+                    let first_number_location = set_iter.next().unwrap();
+                    let first_number_row = matrix[first_number_location.row];
+                    let first_number_str = &first_number_row[first_number_location.start..first_number_location.end];
+                    let first_number = first_number_str.parse::<u32>().unwrap();
+
+                    let second_number_location = set_iter.next().unwrap();
+                    let second_number_row = matrix[second_number_location.row];
+                    let second_number_str = &second_number_row[second_number_location.start..second_number_location.end];
+                    let second_number = second_number_str.parse::<u32>().unwrap();
+
+                    first_number * second_number
+                },
+                Ordering::Greater => panic!(
+                    "More than two numbers found adjacent to asterisk at row {asterisk_row} col {asterisk_col}"
+                ),
+            }
+        })
+        .sum()
+}
+
+pub fn hashmap_locations_no_hashset_gear_ratios(mut input: &str) -> u32 {
     input = input.trim();
 
     let lines: Vec<&str> = input.lines().collect();
 
-    let mut all_locations: HashMap<(u16, u16), Location> = HashMap::new();
+    let mut all_locations: HashMap<(u16, u16), LocationTuple> = HashMap::new();
     for (row, line) in lines.iter().enumerate() {
         let mut previous_byte_offset = 0;
         for part_str in line
@@ -35,8 +125,8 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
                 .map(move |(index, _)| (row as u16, index as u16))
         })
         .filter_map(|(asterisk_row, asterisk_col)| {
-            let mut first: Option<Location> = None;
-            let mut second: Option<Location> = None;
+            let mut first: Option<LocationTuple> = None;
+            let mut second: Option<LocationTuple> = None;
 
             let filtered_adjacent_locations = [
                 (Some(asterisk_row), asterisk_col.checked_add(1)),
@@ -54,26 +144,19 @@ pub fn get_all_gear_ratios_func(mut input: &str) -> u32 {
             for &location in filtered_adjacent_locations {
                 if first.is_none() {
                     first = Some(location);
-                } else if unsafe { first.unwrap_unchecked() } != location {
+                } else if first.unwrap() != location {
                     second = Some(location);
                     break;
                 }
             }
 
-            if first.is_none() || second.is_none() {
-                return None;
-            }
-
-            Some(
-                get_number_from_lines(&lines, first.unwrap())
-                    * get_number_from_lines(&lines, second.unwrap()),
-            )
+            Some(get_number_from_lines(&lines, first?) * get_number_from_lines(&lines, second?))
         })
         .sum()
 }
 
 #[inline(always)]
-fn get_number_from_lines(lines: &[&str], location: Location) -> u32 {
+fn get_number_from_lines(lines: &[&str], location: LocationTuple) -> u32 {
     let (row, start, end) = location;
     lines[row as usize][(start as usize)..(end as usize)]
         .parse::<u32>()
@@ -82,11 +165,11 @@ fn get_number_from_lines(lines: &[&str], location: Location) -> u32 {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use crate::input::INPUT;
 
-    use super::*;
     #[test]
-    fn gear_ratio_func_all_edge_cases() {
+    fn first_impl_all_edge_cases() {
         let input = "
 467..114..
 ...*......
@@ -100,14 +183,41 @@ mod test {
 .664.598..
 ";
 
-        let actual = get_all_gear_ratios_func(input);
+        let actual = first_impl_gear_ratios(input);
 
         assert_eq!(actual, 467835);
     }
 
     #[test]
-    fn gear_ratio_func_real_input() {
-        let actual: u32 = get_all_gear_ratios_func(INPUT);
+    fn first_impl_func_real_input() {
+        let actual: u32 = hashmap_locations_no_hashset_gear_ratios(INPUT);
+
+        assert_eq!(actual, 80253814);
+    }
+
+    #[test]
+    fn hashmap_locations_no_hashset_all_edge_cases() {
+        let input = "
+467..114..
+...*......
+..35..633.
+......#...
+*617......
+.....+.58*
+..592.....
+......755.
+...$.*....
+.664.598..
+";
+
+        let actual = first_impl_gear_ratios(input);
+
+        assert_eq!(actual, 467835);
+    }
+
+    #[test]
+    fn hashmap_locations_no_hashset_real_input() {
+        let actual: u32 = hashmap_locations_no_hashset_gear_ratios(INPUT);
 
         assert_eq!(actual, 80253814);
     }
