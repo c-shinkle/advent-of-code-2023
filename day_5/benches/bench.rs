0049306ae71743c0fa92ea15838de2fdf9e86394
Christian Shinkle <cshinkle2018@gmail.com>
2024-04-01 21:02:42 -0500

    wip

---
+++ day_5/benches/bench.rs
@@ -0,0 +1,53 @@
+#![feature(test)]
+
+extern crate test;
+
+#[cfg(test)]
+mod tests {
+    use day_5::{input, part_1::*};
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part_1_impr_bench(b: &mut Bencher) {
+        let mut sum = u64::default();
+
+        b.iter(|| {
+            sum = black_box(part_1_impr(input::INPUT));
+        });
+
+        assert_eq!(sum, 107430936);
+    }
+
+    #[bench]
+    fn part_1_func_bench(b: &mut Bencher) {
+        let mut sum = u64::default();
+
+        b.iter(|| {
+            sum = black_box(part_1_func(input::INPUT));
+        });
+
+        assert_eq!(sum, 107430936);
+    }
+
+    // #[bench]
+    // fn part_2_impr_bench(b: &mut Bencher) {
+    //     let mut sum = u64::default();
+
+    //     b.iter(|| {
+    //         sum = black_box(part_2_impr(input::INPUT));
+    //     });
+
+    //     assert_eq!(sum, 107430936);
+    // }
+
+    // #[bench]
+    // fn part_2_func_bench(b: &mut Bencher) {
+    //     let mut sum = u32::default();
+
+    //     b.iter(|| {
+    //         sum = black_box(part_2_func(input::INPUT));
+    //     });
+
+    //     assert_eq!(sum, 11024379);
+    // }
+}
--- day_5/src/part_1.rs
+++ day_5/src/part_1.rs
@@ -1,24 +1,32 @@
-use std::{collections::HashMap, str::Lines};
+use itertools::Itertools;
+use std::str::Lines;
 
-fn map_line(lines: &mut Lines<'_>) -> HashMap<u64, u64> {
-    let mut map = HashMap::new();
+fn map_line(lines: &mut Lines<'_>) -> Vec<(u64, u64, u64)> {
+    let mut vec = Vec::new();
     lines.next();
     for line in lines.by_ref().take_while(|line| !line.is_empty()) {
         let mut number_strs = line.split_ascii_whitespace();
         let dest: u64 = number_strs.next().unwrap().parse().unwrap();
-        let source: u64 = number_strs.next().unwrap().parse().unwrap();
+        let src: u64 = number_strs.next().unwrap().parse().unwrap();
         let len: u64 = number_strs.next().unwrap().parse().unwrap();
-        for offset in 0..len {
-            map.insert(source + offset, dest + offset);
+        vec.push((dest, src, len));
+    }
+    vec
+}
+
+fn get_mapping_impr(mappings: &[(u64, u64, u64)], value: u64) -> u64 {
+    for (dest, src, len) in mappings.iter().copied() {
+        if src <= value && value < src + len {
+            return value - src + dest;
         }
     }
-    map
+    value
 }
 
 pub fn part_1_impr(input: &str) -> u64 {
     let mut lines = input.trim().lines();
     let seed_line = lines.next().unwrap();
-    // prep iterator by consuming empty line
+    // prepare iterator by consuming empty line
     lines.next();
 
     let seed_to_soil = map_line(&mut lines);
@@ -29,25 +37,78 @@ pub fn part_1_impr(input: &str) -> u64 {
     let temp_to_humidity = map_line(&mut lines);
     let humidity_to_location = map_line(&mut lines);
 
-    let mut min: Option<u64> = None;
-    let seeds = seed_line[seed_line.find(':').unwrap() + 1..]
+    let mut min = u64::MAX;
+    for seed in seed_line[seed_line.find(':').unwrap() + 1..]
         .split_ascii_whitespace()
-        .map(|seed_str| seed_str.parse().unwrap());
-    for seed in seeds {
-        let soil = seed_to_soil.get(&seed).unwrap_or(&seed);
-        let fertilizer = soil_to_fertilizer.get(soil).unwrap_or(soil);
-        let water = fertilizer_to_water.get(fertilizer).unwrap_or(fertilizer);
-        let light = water_to_light.get(water).unwrap_or(water);
-        let temp = light_to_temp.get(light).unwrap_or(light);
-        let humidity = temp_to_humidity.get(temp).unwrap_or(temp);
-        let location = humidity_to_location.get(humidity).unwrap_or(humidity);
+        .map(|seed_str| seed_str.parse().unwrap())
+    {
+        let soil = get_mapping_impr(&seed_to_soil, seed);
+        let fertilizer = get_mapping_impr(&soil_to_fertilizer, soil);
+        let water = get_mapping_impr(&fertilizer_to_water, fertilizer);
+        let light = get_mapping_impr(&water_to_light, water);
+        let temp = get_mapping_impr(&light_to_temp, light);
+        let humidity = get_mapping_impr(&temp_to_humidity, temp);
+        let location = get_mapping_impr(&humidity_to_location, humidity);
 
-        if min.is_none() || min.unwrap().gt(location) {
-            min = Some(*location);
-        }
+        min = min.min(location);
     }
 
-    min.unwrap()
+    min
+}
+
+pub fn get_mapping_with_next_index(input: &str, skip: usize) -> (usize, Vec<(u64, u64, u64)>) {
+    let mappings: Vec<(usize, (u64, u64, u64))> = input
+        .lines()
+        .enumerate()
+        .skip(skip)
+        .take_while(|(_, line)| !line.is_empty())
+        .map(|(index, line)| {
+            let mapping = line
+                .split_ascii_whitespace()
+                .map(|number_str| number_str.parse().unwrap())
+                .collect_tuple::<(u64, u64, u64)>()
+                .unwrap();
+            (index, mapping)
+        })
+        .collect();
+}
+
+fn get_mapping_func(mappings: &[(u64, u64, u64)], value: u64) -> u64 {
+    mappings
+        .iter()
+        .copied()
+        .find_map(|(dest, src, len)| {
+            (src <= value && value < src + len).then(|| value - src + dest)
+        })
+        .unwrap_or(value)
+}
+
+pub fn part_1_func(input: &str) -> u64 {
+    let input = input.trim();
+    let (skip, seed_to_soil) = get_mapping_with_next_index(input, 3);
+    let (skip, soil_to_fertilizer) = get_mapping_with_next_index(input, skip);
+    let (skip, fertilizer_to_water) = get_mapping_with_next_index(input, skip);
+    let (skip, water_to_light) = get_mapping_with_next_index(input, skip);
+    let (skip, light_to_temp) = get_mapping_with_next_index(input, skip);
+    let (skip, temp_to_humidity) = get_mapping_with_next_index(input, skip);
+    let (_, humidity_to_location) = get_mapping_with_next_index(input, skip);
+
+    let seed_line = input.lines().next().unwrap();
+    seed_line[(seed_line.find(':').unwrap() + 1)..]
+        .split_ascii_whitespace()
+        .map(|seed_str| {
+            let seed = seed_str.parse().unwrap();
+
+            let soil = get_mapping_func(&seed_to_soil, seed);
+            let fertilizer = get_mapping_func(&soil_to_fertilizer, soil);
+            let water = get_mapping_func(&fertilizer_to_water, fertilizer);
+            let light = get_mapping_func(&water_to_light, water);
+            let temp = get_mapping_func(&light_to_temp, light);
+            let humidity = get_mapping_func(&temp_to_humidity, temp);
+            get_mapping_func(&humidity_to_location, humidity)
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -90,15 +151,58 @@ temperature-to-humidity map:
 
 humidity-to-location map:
 60 56 37
-56 93 4";
+56 93 4
+";
 
         let actual = part_1_impr(input);
         assert_eq!(actual, 35);
     }
 
     #[test]
+    fn func_sample() {
+        let input = "
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+";
+
+        let actual = part_1_func(input);
+        assert_eq!(actual, 35);
+    }
+
+    #[test]
     fn impr_puzzle_input() {
-        let actual = part_1_impr(INPUT);
-        assert_eq!(actual, 0);
+        let actual = part_1_func(INPUT);
+        assert_eq!(actual, 107430936);
     }
 }
